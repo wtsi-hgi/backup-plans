dc324ae1fac82cdc88a476d42d2b76369bf2f950
Michael Woolnough <mw31@sanger.ac.uk>
2025-09-19 10:54:30 +0100

    If path hasn't changed, only re-render rules

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -6,11 +6,18 @@ import Rules from './rules.js';
 import { symbols } from './symbols.js';
 
 (document.readyState === "complete" ? Promise.resolve() : new Promise(successFn => window.addEventListener("load", successFn, { "once": true }))).then(() => {
+	let lastPath = "";
+
 	const load = (path: string) => Load(path).then(data => {
-		Breadcrumbs.update(path, load);
-		DiskTree.update(path, data, load);
-		DirInfo.update(path, data, load);
+		if (path !== lastPath) {
+			Breadcrumbs.update(path, load);
+			DiskTree.update(path, data, load);
+			DirInfo.update(path, data, load);
+		}
+
 		Rules.update(path, data, load);
+
+		lastPath = path;
 	});
 
 	return load("/");
