0d13b267b019e4013b337d126777f99dd7a5d6e3
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-09-22 09:46:27 +0100

    wip

--- gitignore/gitignore.go
+++ gitignore/gitignore.go
@@ -1,22 +1,48 @@
 package gitignore
 
 import (
+	"os"
+	"strings"
+
 	parser "github.com/sabhiram/go-gitignore"
 )
 
-type gi struct {
-	Matcher *parser.GitIgnore
+type GitIgnore struct {
+	Matcher     *parser.GitIgnore
+	IgnoreLines []string
+}
+
+// Given a gitIgnore filepath, returns a gitignore object.
+func New(path string) (gi *GitIgnore, err error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	g := &GitIgnore{}
+	g.Matcher = parser.CompileIgnoreLines(string(file))
+	g.IgnoreLines = strings.Split(string(file), "\n")
+
+	return g, nil
 }
 
 // Returns subset slices ignore and keep of the input, dividing up each input
 // path into files to ignore and files to backup
-func (g *gi) Match(paths []string) (ignore []string, keep []string) {
+func (g *GitIgnore) Match(paths []string) (ignore []string, keep []string) {
 	return nil, nil
 }
 
-// Given a gitIgnore filepath, returns a gitignore scheme
-func New(path string) (gi gi, err error) {
-	matcher := parser.CompileIgnoreLines(path)
-	gi.Matcher = matcher
-	return gi, nil
+// Given a gitignore object, returns gitignore rules
+func (g *GitIgnore) GetRules() ([]string, error) {
+	return nil, nil
+}
+
+// Given a gitignore object, rules can be removed
+func (g *GitIgnore) RemoveRules([]string) error {
+	return nil
+}
+
+// Given a gitignore object, rules can be added
+func (g *GitIgnore) AddRules([]string) error {
+	return nil
 }
--- gitignore/gitignore_test.go
+++ gitignore/gitignore_test.go
@@ -7,16 +7,39 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	Convey("Given a gitIgnore filepath, a matcher can be retrieved", t, func() {
-		gi, err := New("path/gitignore.txt")
+	Convey("Given a gitIgnore filepath, a gitignore object with a valid matcher can be retrieved", t, func() {
+		gi, err := New("gitignoreExample.txt")
 		So(gi, ShouldNotBeNil)
 		So(err, ShouldBeNil)
 
-		Convey("Given a matcher, files can be split into ignore/keep", func() {
-			var paths []string
+		Convey("Files can be split into ignore/keep", func() {
+			paths := []string{"testfile.txt", "testfile2.log"}
 			ignore, keep := gi.Match(paths)
-			So(ignore, ShouldNotBeNil)
-			So(keep, ShouldNotBeNil)
+			So(ignore, ShouldEqual, []string{"testfile2.log"})
+			So(keep, ShouldEqual, []string{"testfile.txt"}) //TODO: check with CASM what behaviour they have in mind for files not matched by any rule
+		})
+
+		Convey("Rules can be retrieved", func() {
+			rules, err := gi.GetRules()
+			So(err, ShouldBeNil)
+			So(rules, ShouldEqual, []string{"*.log", "/build/", "/!important.log"})
+		})
+
+		Convey("Rules can be added", func() {
+			err = gi.AddRules([]string{"*.txt", "/test/"})
+			So(err, ShouldBeNil)
+			newrules, err := gi.GetRules()
+			So(err, ShouldBeNil)
+			So(newrules, ShouldEqual, []string{"*.log", "/build/", "/!important.log", "*.txt", "/test/"})
+		})
+
+		Convey("Rules can be removed", func() {
+			err = gi.RemoveRules([]string{"*.log"})
+			So(err, ShouldBeNil)
+
+			newrules, err := gi.GetRules()
+			So(err, ShouldBeNil)
+			So(newrules, ShouldEqual, []string{"/build/", "/!important.log"})
 		})
 	})
 }
