741fce2dca208848595b59e635f0684cbe8a4b0d
Prasannakumar Nuniganti <pn10@sanger.ac.uk>
2025-10-20 16:16:38 +0100

    Fix the report width size and re-embed

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -13,4 +13,4 @@ import (
 //go:embed index.html.gz
 var indexHTML []byte
 
-var Index = httpembed.HandleBuffer("index.html", indexHTML, 40118, time.Unix(1760973148, 0)) //nolint:gochecknoglobals,lll,mnd
+var Index = httpembed.HandleBuffer("index.html", indexHTML, 40134, time.Unix(1760973362, 0)) //nolint:gochecknoglobals,lll,mnd
