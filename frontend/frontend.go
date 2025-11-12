4757f2cfd32a3551fda5e88f9166a103015f3f61
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-11-12 15:12:27 +0000

    Rebuild frontend

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -16,13 +16,8 @@ import (
 var indexHTML []byte
 
 const (
-<<<<<<< HEAD
-	uncompressedSize = 47440
+	uncompressedSize = 49430
 	lastModifiedTime = 1763050493
-=======
-	uncompressedSize = 47082
-	lastModifiedTime = 1762873734
->>>>>>> 0b78155 (Allow wildcards in fofns)
 )
 
 var Index = httpembed.HandleBuffer("index.html", indexHTML, uncompressedSize, time.Unix(lastModifiedTime, 0)) //nolint:gochecknoglobals,lll
