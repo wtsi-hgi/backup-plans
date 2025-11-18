0ccadeab26c355dbcd8ccfef0ef7ec7047948a51
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-11-18 09:41:59 +0000

    Remove rule styling

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -16,8 +16,8 @@ import (
 var indexHTML []byte
 
 const (
-	uncompressedSize = 47106
-	lastModifiedTime = 1763381262
+	uncompressedSize = 47265
+	lastModifiedTime = 1763458872
 )
 
 var Index = httpembed.HandleBuffer("index.html", indexHTML, uncompressedSize, time.Unix(lastModifiedTime, 0)) //nolint:gochecknoglobals,lll
