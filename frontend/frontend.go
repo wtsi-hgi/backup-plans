90abc32a3f25929d8e26e377c3c3778639893467
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-11-21 16:03:53 +0000

    Remove file type specification

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -16,8 +16,8 @@ import (
 var indexHTML []byte
 
 const (
-	uncompressedSize = 51039
-	lastModifiedTime = 1763736475
+	uncompressedSize = 51025
+	lastModifiedTime = 1763741012
 )
 
 var Index = httpembed.HandleBuffer("index.html", indexHTML, uncompressedSize, time.Unix(lastModifiedTime, 0)) //nolint:gochecknoglobals,lll
