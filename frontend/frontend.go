a6dd762c043ffd38f1916c46b4c567b984e95472
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-11-21 11:59:57 +0000

    Round dates correctly

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -17,7 +17,7 @@ var indexHTML []byte
 
 const (
 	uncompressedSize = 50935
-	lastModifiedTime = 1763649001
+	lastModifiedTime = 1763726368
 )
 
 var Index = httpembed.HandleBuffer("index.html", indexHTML, uncompressedSize, time.Unix(lastModifiedTime, 0)) //nolint:gochecknoglobals,lll
