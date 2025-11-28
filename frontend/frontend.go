ccf14eceb88a323007e3a98fc4922e3ff130f3d0
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-11-28 11:51:13 +0000

    Fix rebase; open file bug

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -15,23 +15,8 @@ import (
 var indexHTML []byte
 
 const (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	uncompressedSize = 52736
-	lastModifiedTime = 1764263373
-=======
 	uncompressedSize = 51839
 	lastModifiedTime = 1764250278
->>>>>>> c24f4c4 (Allow relative paths in FOFNs)
-=======
-	uncompressedSize = 51841
-	lastModifiedTime = 1764250638
->>>>>>> f7c702f (Allow full relative paths)
-=======
-	uncompressedSize = 52008
-	lastModifiedTime = 1764253210
->>>>>>> 671fade (Allow relative paths with prefix ./)
 )
 
 var Index = httpembed.HandleBuffer("index.html", indexHTML, uncompressedSize, time.Unix(lastModifiedTime, 0)) //nolint:gochecknoglobals,lll
