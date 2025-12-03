b17fdd2163aa2871a855a23ff87e748bb28388a6
Sky Haines-Bass <sh55@sanger.ac.uk>
2025-12-03 16:47:23 +0000

    Fix ui; setExists alert

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,21 +1,94 @@
-import type { BackupType } from "./types.js";
+export class BackupType extends Number {
+    static BackupWarn = new BackupType(-1);
+    static BackupNone = new BackupType(0);
+    static BackupIBackup = new BackupType(1);
+    static BackupManualIBackup = new BackupType(2);
+    static BackupManualGit = new BackupType(3);
+    static BackupManualUnchecked = new BackupType(4);
+    static BackupManualPrefect = new BackupType(5);
 
-export const [BackupNone, BackupIBackup, BackupManualIBackup, BackupManualGit, BackupManualUnchecked, BackupManualPrefect] = Array.from({ "length": 6 }, (_, n) => n as BackupType),
-    BackupWarn = -1;
+    static all = [BackupType.BackupNone, BackupType.BackupIBackup, BackupType.BackupManualIBackup, BackupType.BackupManualGit, BackupType.BackupManualUnchecked, BackupType.BackupManualPrefect];
+    static manual = [BackupType.BackupManualIBackup, BackupType.BackupManualGit, BackupType.BackupManualUnchecked, BackupType.BackupManualPrefect];
 
-export const ManualBackupTypes = [BackupManualIBackup, BackupManualGit, BackupManualPrefect, BackupManualUnchecked];
-export const ManualBackupStrings = ["manualibackup", "manualgit", "manualprefect", "manualunchecked"];
+    static from(bt: string | number | BackupType) {
+        switch (bt) {
+            case +BackupType.BackupNone:
+            case "nobackup":
+                return BackupType.BackupNone;
+            case +BackupType.BackupIBackup:
+            case "backup":
+                return BackupType.BackupIBackup;
+            case +BackupType.BackupManualIBackup:
+            case "manualibackup":
+                return BackupType.BackupManualIBackup;
+            case +BackupType.BackupManualGit:
+            case "manualgit":
+                return BackupType.BackupManualGit
+            case +BackupType.BackupManualUnchecked:
+            case "manualunchecked":
+                return BackupType.BackupManualUnchecked;
+            case +BackupType.BackupManualPrefect:
+            case "manualprefect":
+                return BackupType.BackupManualPrefect
+        }
 
-export const ManualBackupString = {
-    "ManualBackup": "manualibackup",
-    "ManualGit": "manualgit",
-    "ManualPrefect": "manualprefect",
-    "ManualUnchecked": "manualunchecked"
-} as const;
+        throw new Error("invalid backup type");
+    }
 
-export const ManualBackupDisplay = {
-    "ManualBackup": "Manual Backup: iBackup",
-    "ManualGit": "Manual Backup: Git",
-    "ManualPrefect": "Manual Backup: Prefect",
-    "ManualUnchecked": "Manual Backup: Unchecked"
+    toString() {
+        switch (this) {
+            case BackupType.BackupNone:
+                return "nobackup";
+            case BackupType.BackupIBackup:
+                return "backup";
+            case BackupType.BackupManualIBackup:
+                return "manualibackup";
+            case BackupType.BackupManualGit:
+                return "manualgit";
+            case BackupType.BackupManualUnchecked:
+                return "manualunchecked";
+            case BackupType.BackupManualPrefect:
+                return "manualprefect";
+        }
+
+        return ""
+    }
+
+    optionLabel() {
+        switch (+this) {
+            case +BackupType.BackupNone:
+                return "No Backup";
+            case +BackupType.BackupIBackup:
+                return "iBackup";
+            case +BackupType.BackupManualIBackup:
+                return "Manual Backup: iBackup";
+            case +BackupType.BackupManualGit:
+                return "Manual Backup: Git";
+            case +BackupType.BackupManualUnchecked:
+                return "Manual Backup: Unchecked";
+            case +BackupType.BackupManualPrefect:
+                return "Manual Backup: Prefect";
+        }
+
+        return "";
+    }
+
+    metadataLabel() {
+        switch (+this) {
+            case +BackupType.BackupManualIBackup:
+                return "Set Name";
+            case +BackupType.BackupManualGit:
+                return "Git URL";
+            case +BackupType.BackupManualUnchecked:
+                return "Metadata";
+            case +BackupType.BackupManualPrefect:
+                return "Prefect URL";
+        }
+
+        return "aaa";
+    }
+
+    isManual() {
+        return +this >= +BackupType.BackupManualIBackup;
+    }
 }
