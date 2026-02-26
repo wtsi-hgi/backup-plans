d9cb2b8749a5a672ed712827d0080620f0745cf0
sb10 <sb10@users.noreply.github.com>
2026-02-26 07:45:04 +0000

    Rebuilt Frontend

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -15,8 +15,8 @@ import (
 var indexHTML []byte
 
 const (
-	uncompressedSize = 285427
-	lastModifiedTime = 1771939044
+	uncompressedSize = 285424
+	lastModifiedTime = 1772091902
 )
 
 var Index = httpembed.HandleBuffer("index.html", indexHTML, uncompressedSize, time.Unix(lastModifiedTime, 0)) //nolint:gochecknoglobals,lll
