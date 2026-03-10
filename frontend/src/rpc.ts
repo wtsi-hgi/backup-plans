7659bdbee8326791cc8b9102d277ddebd90044d1
Michael Woolnough <130465766+mjkw31@users.noreply.github.com>
2026-03-10 13:56:55 +0000

    Change mutation endpoints to use POST requests (#114)

--- frontend/src/rpc.ts
+++ frontend/src/rpc.ts
@@ -1,48 +1,44 @@
 import type { ReportSummary, Tree, UserGroups, DirStats } from './types.js';
 
-const getURL = <T>(url: string, params: Record<string, unknown> = {}, body?: string) => {
-	return new Promise<T>((successFn, errorFn) => {
-		const urlParams: string[] = [];
+const encodeForm = (params: Record<string, unknown>) => new URLSearchParams(Object.entries(params as Record<string, string>)),
+	getURL = <T>(url: string, params: Record<string, string> = {}, body?: string | Record<string, unknown>) => {
+		return new Promise<T>((successFn, errorFn) => {
+			const urlParams = encodeForm(params).toString(),
+				xh = new XMLHttpRequest();
 
-		for (const [key, value] of Object.entries(params)) {
-			urlParams.push(`${key}=${encodeURIComponent(value + "")}`);
-		}
-
-		if (urlParams.length) {
-			url += "?" + urlParams.join("&");
-		}
-
-		const xh = new XMLHttpRequest();
-
-		xh.open(body ? "POST" : "GET", url);
-		xh.addEventListener("readystatechange", () => {
-			if (xh.readyState === 4) {
-				if (xh.status === 204 || !xh.responseText.length) {
-					successFn(null as T);
-				} else if (xh.status === 200) {
-					successFn(JSON.parse(xh.responseText));
-				} else {
-					errorFn(new Error(xh.responseText));
-				}
+			if (urlParams.length) {
+				url += "?" + urlParams;
 			}
+
+			xh.open(body ? "POST" : "GET", url);
+			xh.addEventListener("readystatechange", () => {
+				if (xh.readyState === xh.DONE) {
+					if (xh.status === 204 || !xh.responseText.length) {
+						successFn(null as T);
+					} else if (xh.status === 200) {
+						successFn(JSON.parse(xh.responseText));
+					} else {
+						errorFn(new Error(xh.responseText));
+					}
+				}
+			});
+			xh.send(body ? typeof body === "string" ? body : encodeForm(body) : null);
 		});
-		xh.send(body);
-	});
-};
+	};
 
 export const getTree = (dir: string) => getURL<Tree>("api/tree", { dir }),
-	claimDir = (dir: string) => getURL<void>("api/dir/claim", { dir }),
-	passDirClaim = (dir: string, passTo: string) => getURL<void>("api/dir/pass", { dir, passTo }),
-	revokeDirClaim = (dir: string) => getURL<void>("api/dir/revoke", { dir }),
-	createRule = (dir: string, action: string, match: string, metadata: string, override: boolean) => getURL<void>("api/rules/create", { dir, action, match, metadata, override }),
-	updateRule = (dir: string, action: string, match: string, metadata: string, override: boolean) => getURL<void>("api/rules/update", { dir, action, match, metadata, override }),
-	removeRule = (dir: string, match: string) => getURL<void>("api/rules/remove", { dir, match }),
+	claimDir = (dir: string) => getURL<void>("api/dir/claim", {}, { dir }),
+	passDirClaim = (dir: string, passTo: string) => getURL<void>("api/dir/pass", {}, { dir, passTo }),
+	revokeDirClaim = (dir: string) => getURL<void>("api/dir/revoke", {}, { dir }),
+	createRule = (dir: string, action: string, match: string, metadata: string, override: boolean) => getURL<void>("api/rules/create", {}, { dir, action, match, metadata, override }),
+	updateRule = (dir: string, action: string, match: string, metadata: string, override: boolean) => getURL<void>("api/rules/update", {}, { dir, action, match, metadata, override }),
+	removeRule = (dir: string, match: string) => getURL<void>("api/rules/remove", {}, { dir, match }),
 	getReportSummary = () => getURL<ReportSummary>("api/report/summary"),
 	uploadFOFN = (dir: string, action: string, metadata: string, files: string[]) => getURL<void>("api/uploadfofn", { dir, action, metadata }, JSON.stringify(files)),
-	setDirDetails = (dir: string, frequency: number, frozen: boolean, review: number, remove: number) => getURL<void>("api/dir/setdetails", { dir, frequency, frozen, review, remove }),
+	setDirDetails = (dir: string, frequency: number, frozen: boolean, review: number, remove: number) => getURL<void>("api/dir/setdetails", {}, { dir, frequency, frozen, review, remove }),
 	setExists = (dir: string, metadata: string) => getURL<boolean>("api/setExists", { dir, metadata }),
 	getDirectories = (paths: string[]) => getURL<boolean[]>("api/getDirectories", {}, JSON.stringify(paths)),
 	getUserGroups = () => getURL<UserGroups>("api/usergroups"),
 	getMainProgrammes = () => getURL<string[]>("api/mainprogrammes"),
-	getClaimStats = (user: string, groupbom: string) => getURL<DirStats[]>("api/claimstats", { user, groupbom }),
+	getClaimStats = (user: string, groupbom: string) => getURL<DirStats[]>("api/claimstats", {}, { user, groupbom }),
 	user = await getURL<string>("api/whoami");
