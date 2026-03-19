74753d3f8ee71633965904e17f63322c354d1297
Sky Haines-Bass <sh55@sanger.ac.uk>
2026-03-19 12:56:34 +0000

    Cleanup

--- internal/wrstat/wrstat.go
+++ internal/wrstat/wrstat.go
@@ -7,9 +7,8 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey" //nolint:revive,staticcheck
 	"github.com/wtsi-hgi/backup-plans/internal/directories"
-	"github.com/wtsi-hgi/backup-plans/internal/plandb"
 	wr "github.com/wtsi-hgi/backup-plans/wrstat"
 	gas "github.com/wtsi-hgi/go-authserver"
 	"github.com/wtsi-hgi/wrstat-ui/basedirs"
@@ -28,10 +27,10 @@ const (
 	dgutaPath           = "dguta.dbs"
 )
 
-func NewTestWRStatClient(t *testing.T, tree *directories.Root) *wr.Client {
+func NewTestWRStatClient(t *testing.T, tree *directories.Root) *wr.Client { //nolint:funlen
 	t.Helper()
 
-	s := summary.NewSummariser(stats.NewStatsParser(plandb.ExampleTree().AsReader()))
+	s := summary.NewSummariser(stats.NewStatsParser(tree.AsReader()))
 
 	tmp := t.TempDir()
 	run := filepath.Join(tmp, "20251225-000000_root")
@@ -40,14 +39,14 @@ func NewTestWRStatClient(t *testing.T, tree *directories.Root) *wr.Client {
 
 	t.Setenv("XDG_STATE_HOME", tmp)
 
-	So(os.MkdirAll(dguta, 0700), ShouldBeNil)
-	So(os.WriteFile(owners, nil, 0600), ShouldBeNil)
+	So(os.MkdirAll(dguta, 0700), ShouldBeNil)        //nolint:mnd
+	So(os.WriteFile(owners, nil, 0600), ShouldBeNil) //nolint:mnd
 
 	db := db.NewDB(dguta)
 
 	So(db.CreateDB(), ShouldBeNil)
 
-	db.SetBatchSize(100)
+	db.SetBatchSize(100) //nolint:mnd
 	s.AddDirectoryOperation(dirguta.NewDirGroupUserTypeAge(db))
 
 	bd, err := basedirs.NewCreator(filepath.Join(run, basedirsPath), nil)
