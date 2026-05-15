c05e38b1706d4d126163cf60639bdaa60c5f6a36
Michael Woolnough <130465766+mjkw31@users.noreply.github.com>
2026-05-15 10:36:04 +0100

    Don't pass up non-fatal errors (#145)

--- backups/backups.go
+++ backups/backups.go
@@ -184,7 +184,8 @@ func collectRuleGroups(root *ruletree.RuleTree, path string, rules ruletree.Rule
 }
 
 func figureOutFOFNs(node tree.Node, sm ruletree.State, path *summary.DirectoryPath,
-	cb func(*summary.DirectoryPath, int64, int64)) {
+	cb func(*summary.DirectoryPath, int64, int64),
+) {
 	for name, child := range node.Children() {
 		state := sm.GetStateString(name)
 		newPath := &summary.DirectoryPath{Parent: path, Name: name}
@@ -233,10 +234,11 @@ func addFofnsToIBackup(client backupClient, setFofns map[*db.Directory][]server.
 			continue
 		}
 
-		err = client.Backup(setInfo.Path, backupSetName, setInfo.ClaimedBy, fofns,
-			int(setInfo.Frequency), frozen, setInfo.ReviewDate, setInfo.RemoveDate) //nolint:gosec
-		if err != nil {
-			errs = errors.Join(errs, err)
+		if err = client.Backup(setInfo.Path, backupSetName, setInfo.ClaimedBy, fofns,
+			int(setInfo.Frequency), frozen, setInfo.ReviewDate, setInfo.RemoveDate); err != nil { //nolint:gosec
+			if !errors.Is(err, ibackup.ErrNoUpdate) && !errors.Is(err, ibackup.ErrUnknownClient) {
+				errs = errors.Join(errs, err)
+			}
 
 			continue
 		}
